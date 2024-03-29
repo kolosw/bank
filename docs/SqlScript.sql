da2a799164bec8fd0ac89786e5c17d1aaac2bcd5
kolosw <mr.maddara@gmail.com>
2024-03-29 15:39:01 +0300

    Changed docs

--- docs/SqlScript.sql
+++ docs/SqlScript.sql
@@ -13,7 +13,7 @@ shortname VARCHAR(32) NOT NULL,
 symbol VARCHAR(1) NOT NULL
 );
 
-CREATE TABLE accounts(
+CREATE TABLE account(
 id SERIAL PRIMARY KEY,
 user_id INT NOT NULL REFERENCES users(id),
 type VARCHAR(32) NOT NULL
@@ -21,30 +21,29 @@ type VARCHAR(32) NOT NULL
 
 CREATE TABLE account_currency(
 id SERIAL PRIMARY KEY,
-account_id INT NOT NULL REFERENCES accounts(id),
+account_id INT NOT NULL REFERENCES account(id),
 currency_id INT NOT NULL REFERENCES currencies(id),
 balance INT NOT NULL
 );
 
 CREATE TABLE transactions(
 id SERIAL PRIMARY KEY,
-sender_id INT NOT NULL REFERENCES accounts(id),
-recipient_id INT NOT NULL REFERENCES accounts(id),
+sender_id INT NOT NULL REFERENCES account(id),
+recipient_id INT NOT NULL REFERENCES account(id),
 date DATE NOT NULL,
 time TIME NOT NULL,
-currency_id INT NOT NULL REFERENCES currencies(id),
 amount INT NOT NULL
 );
 
 CREATE TABLE beneficiaries(
 id SERIAL PRIMARY KEY,
 name INT NOT NULL,
-account_id INT NOT NULL REFERENCES users(id)
+account_id INT NOT NULL REFERENCES account(id)
 );
 
 CREATE TABLE currency_exchange(
 id SERIAL PRIMARY KEY,
 from_id INT NOT NULL REFERENCES currencies(id),
-to_id INT NOT NULL,
+to_id INT NOT NULL REFERENCES currencies(id),
 amount FLOAT NOT NULL
 )
